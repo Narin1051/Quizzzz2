492746bf2c2d15c14493035ba3f6743389e3d37a
Narin1051 <narin.phue@mail.kmutt.ac.th>
2023-04-03 10:28:38 +0700

    Fin

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿class program{
+
+    static void Main(string[] args){
+        List<double> numbers = new List<double>();
+            string input = (Console.ReadLine());           
+            
+    while ((input = Console.ReadLine()) != "End")
+        {
+            double number;
+            if (double.TryParse(input, out number))
+            {
+                numbers.Add(number);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input.");
+            }
+        }
+    
+       while ((input = Console.ReadLine()) != "")
+        {
+            switch (input)
+            {
+                case "FindMax":
+                    Console.WriteLine($"Maximum value: {numbers.Max()}");
+                    break;
+
+                case "FindMin":
+                    Console.WriteLine($"Minimum value: {numbers.Min()}");
+                    break;
+
+                case "FindMean":
+                    Console.WriteLine($"Mean value: {numbers.Average()}");
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid mode.");
+                    break;
+            }
+
+        }
+    }   
+}
